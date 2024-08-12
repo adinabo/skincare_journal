eac8251dfb8f15987de1955291ba75678170cdd5
Adina Bobocioiu <adina.bobocioiu@gmail.com>
2024-08-12 19:01:46 +0000

    created the first models sketch

--- skincare_package/models.py
+++ skincare_package/models.py
@@ -1,1 +1,17 @@
 from skincare_package import db
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+# Initialize the MongoDB client and select the database
+client = MongoClient('mongodb://localhost:27017/')
+db = client['Skincare_db']
+
+# For collection: users
+users_collection = db['users']
+
+# Collection: users_routines
+users_routines_collection = db['users_routines']
+
+# Collection: skintype
+skintype_collection = db['skintype']
+
