247b5f968dd11af7bcc93e9af305aa5db9aca5dc
Adina Bobocioiu <adina.bobocioiu@gmail.com>
2024-08-14 11:21:49 +0000

    code tidy-up removed comments

--- run.py
+++ run.py
@@ -54,35 +54,6 @@ def profile():
     else:
         return redirect(url_for("login"))
 
-    
-#def seed_database():
-    # Check if users collection is empty
-#    if models.users_collection.count_documents({}) == 0:
-#        print("Seeding initial data...")
-
-        # Create a new user
-#        user_id = models.create_user("john_doe", "john@example.com", "hashed_password").inserted_id
-
-        # Create a new skin type
-#        skin_type_id = models.create_skin_type(
- #           "oily",
-#            "Skin produces excess sebum, often with enlarged pores and a shiny appearance.",
- #           ["acne", "blackheads", "oily T-zone"],
- #           ["oil-free cleansers", "mattifying moisturizers"]
- #       ).inserted_id
-
-        # Create a new routine for the user
-#        models.create_user_routine(
- #           user_id,
- #           skin_type_id,
-#            "Cleanser: CeraVe Hydrating Cleanser: Removes makeup and impurities without stripping the skin."
-#        )
-
-#        print("User and routine created successfully")
-#    else:
- #       print("Data already exists in the database. Skipping seeding.")
-
-#seed_database()
 
 if __name__ == "__main__":
     # Run the Flask application
